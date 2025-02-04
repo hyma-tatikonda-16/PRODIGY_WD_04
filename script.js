467331d01c4c5690510cb4fccc3d1cb043b50e94
hyma-tatikonda-16 <hymatatikonda16@gmail.com>
2025-02-04 19:01:28 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // You can add more interactive functionality here, like form validation or animations.
+    console.log("Portfolio site loaded!");
+});
